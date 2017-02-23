2c96b6a2347dbc19f1343c8b6056d537d2c63fcc
a.sushchikh <a.sushchikh@gmail.com>
2017-02-23 07:43:22 +0300

    add all files

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+$recepient = "sushchikh@yandex.ru";
+$sitename = "Прицепы «Целина»";
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон: $phone \ne-mail: $email";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+
+
